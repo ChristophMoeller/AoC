e709c6ce5640c32e4c174c28e7da158b3c37d5c4
Christoph Moeller <chris.moeller99@gmail.com>
2023-12-08 16:17:44 +0100

    2023, Day 07 and 08

--- year2023/src/day07.rs
+++ year2023/src/day07.rs
@@ -1,29 +1,250 @@
+use std::cmp::Ordering;
+
 use super::*;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Card {
+    Number(u32),
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+impl Card {
+    fn index(&self) -> usize {
+        match *self {
+            Card::Number(d) => (d - 2) as usize,
+            Card::Jack => 9 as usize,
+            Card::Queen => 10 as usize,
+            Card::King => 11 as usize,
+            Card::Ace => 12 as usize,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum Type {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+impl Card {
+    fn type_of_hand(cards: &[Card; 5]) -> Type {
+        let mut amount = [0; 13];
+
+        for card in cards {
+            amount[card.index()] += 1;
+        }
+
+        for n in amount {
+            if n == 5 {
+                return Type::FiveOfAKind;
+            }
+            if n == 4 {
+                return Type::FourOfAKind;
+            }
+            if n == 3 {
+                for m in amount {
+                    if m == 2 {
+                        return Type::FullHouse;
+                    }
+                }
+
+                return Type::ThreeOfAKind;
+            }
+        }
+
+        let mut pairs = 0;
+        for n in amount {
+            if n == 2 {
+                pairs += 1;
+            }
+        }
+        if pairs == 2 {
+            return Type::TwoPair;
+        }
+        if pairs == 1 {
+            return Type::OnePair;
+        }
+
+        Type::HighCard
+    }
+
+    fn type_of_hand_joker(cards: &[Card; 5]) -> Type {
+        let pos = cards.iter().position(|x| *x == Card::Jack);
+
+        let Some(pos) = pos else {
+            return Card::type_of_hand(cards);
+        };
+
+        let cards_excepts_joker = [
+            Card::Ace,
+            Card::King,
+            Card::Queen,
+            Card::Number(10),
+            Card::Number(9),
+            Card::Number(8),
+            Card::Number(7),
+            Card::Number(6),
+            Card::Number(5),
+            Card::Number(4),
+            Card::Number(3),
+            Card::Number(2),
+        ];
+
+        let mut cards = cards.clone();
+        let mut max_type = Type::HighCard;
+        for card in cards_excepts_joker {
+            cards[pos] = card;
+            let t = Self::type_of_hand_joker(&cards);
+            max_type = max_type.max(t);
+        }
+
+        max_type
+    }
+
+    fn compare_hands(a: &[Card; 5], b: &[Card; 5]) -> std::cmp::Ordering {
+        Card::type_of_hand(a).cmp(&Card::type_of_hand(b)).then(
+            a.iter()
+                .zip(b.iter())
+                .fold(Ordering::Equal, |acc, (a, b)| acc.then(a.cmp(b))),
+        )
+    }
+}
+
+impl Card {
+    fn cmp_joker(&self, other: &Card) -> Ordering {
+        if *self == Card::Jack || *other == Card::Jack {
+            if self == other {
+                Ordering::Equal
+            } else if *self == Card::Jack {
+                Ordering::Less
+            } else {
+                Ordering::Greater
+            }
+        } else {
+            self.cmp(other)
+        }
+    }
+
+    fn compare_hands_joker(a: &[Card; 5], b: &[Card; 5]) -> std::cmp::Ordering {
+        Card::type_of_hand_joker(a)
+            .cmp(&Card::type_of_hand_joker(b))
+            .then(
+                a.iter()
+                    .zip(b.iter())
+                    .fold(Ordering::Equal, |acc, (a, b)| acc.then(a.cmp_joker(b))),
+            )
+    }
+}
 
 pub struct Day07;
 impl Solution for Day07 {
-    type Input<'a> = ();
+    type Input<'a> = Vec<([Card; 5], u32)>;
 
     fn parse<'a>(content: &'a str) -> Self::Input<'a> {
         parsing::parse(content).unwrap().1
     }
 
-    fn part_a<'a>(_input: &Self::Input<'a>) -> String {
-        format!("{}", 0)
+    fn part_a<'a>(input: &Self::Input<'a>) -> String {
+        let mut hand = input.clone();
+
+        hand.sort_by(|(x, _), (y, _)| Card::compare_hands(x, y));
+
+        let sum: u64 = hand
+            .iter()
+            .enumerate()
+            .map(|(i, (_, bid))| (i as u64 + 1) * (*bid as u64))
+            .sum();
+
+        format!("{}", sum)
     }
 
-    fn part_b<'a>(_input: &Self::Input<'a>) -> String {
-        format!("{}", 0)
+    fn part_b<'a>(input: &Self::Input<'a>) -> String {
+        let mut hand = input.clone();
+
+        hand.sort_by(|(x, _), (y, _)| Card::compare_hands_joker(x, y));
+
+        let sum: u64 = hand
+            .iter()
+            .enumerate()
+            .map(|(i, (_, bid))| (i as u64 + 1) * (*bid as u64))
+            .sum();
+
+        format!("{}", sum)
     }
 }
 
 mod parsing {
-    use nom::*;
+    use super::Card;
 
-    pub(super) fn parse(input: &str) -> IResult<&str, ()> {
-        Ok((input, ()))
+    use nom::{
+        bytes::complete::take_while1,
+        character::complete::{anychar, line_ending, space1, u32},
+        multi::{many1, separated_list1},
+        sequence::separated_pair,
+        *,
+    };
+
+    fn card(input: &str) -> IResult<&str, Card> {
+        anychar
+            .map(|c| match c {
+                'A' => Card::Ace,
+                'K' => Card::King,
+                'Q' => Card::Queen,
+                'J' => Card::Jack,
+                'T' => Card::Number(10),
+                '9' => Card::Number(9),
+                '8' => Card::Number(8),
+                '7' => Card::Number(7),
+                '6' => Card::Number(6),
+                '5' => Card::Number(5),
+                '4' => Card::Number(4),
+                '3' => Card::Number(3),
+                '2' => Card::Number(2),
+                _ => unimplemented!(),
+            })
+            .parse(input)
+    }
+
+    fn hand(input: &str) -> IResult<&str, [Card; 5]> {
+        take_while1(|c: char| c.is_alphanumeric())
+            .and_then(many1(card))
+            .map(|x| {
+                assert!(x.len() >= 5);
+                [x[0], x[1], x[2], x[3], x[4]]
+            })
+            .parse(input)
+    }
+
+    pub(super) fn parse(input: &str) -> IResult<&str, Vec<([Card; 5], u32)>> {
+        separated_list1(line_ending, separated_pair(hand, space1, u32))(input)
     }
 }
 
-// gen_test!(a, Day07, r"", "");
-// gen_test!(b, Day07, r"", "");
+gen_test!(
+    a,
+    Day07,
+    r"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483",
+    "6440"
+);
+gen_test!(
+    b,
+    Day07,
+    r"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483",
+    "5905"
+);
--- year2023/src/day08.rs
+++ year2023/src/day08.rs
@@ -1,29 +1,295 @@
+use itertools::Itertools;
+use num_integer::{ExtendedGcd, Integer};
+use std::collections::HashMap;
+
 use super::*;
+
+#[derive(Clone, Copy, Debug)]
+pub enum Direction {
+    Left,
+    Right,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Node {
+    left: u32,
+    right: u32,
+}
+
+impl Node {
+    fn get(&self, direction: Direction) -> u32 {
+        match direction {
+            Direction::Left => self.left,
+            Direction::Right => self.right,
+        }
+    }
+
+    fn index(label: &str) -> u32 {
+        let label = label.as_bytes();
+        (label[0] as u32) * 256 * 256 + (label[1] as u32) * 256 + label[2] as u32
+    }
+
+    fn is_start(node: u32) -> bool {
+        node % 256 == 'A' as u32
+    }
+
+    fn is_end(node: u32) -> bool {
+        node % 256 == 'Z' as u32
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Cycle {
+    offset: u64,
+    length: u64,
+    accepted_start: Vec<u64>,
+    accepted_cycle: Vec<u64>,
+}
+
+impl Cycle {
+    fn trace(instructions: &[Direction], graph: &HashMap<u32, Node>, start: u32) -> Self {
+        let mut current = start;
+        let mut steps = 0;
+
+        let instruction_count = instructions.len() as u64;
+        let mut instructions = instructions.iter().cycle().copied();
+
+        let mut accepted = Vec::new();
+        let mut visited = HashMap::new();
+        visited.insert(start, 0);
+
+        let offset = loop {
+            steps += 1;
+
+            current = graph[&current].get(instructions.next().unwrap());
+            if Node::is_end(current) {
+                accepted.push(steps);
+            }
+
+            let offset = *visited.entry(current).or_insert(steps);
+            if steps > offset && (steps - offset) % instruction_count == 0 {
+                break offset;
+            }
+        };
+
+        let length = steps - offset;
+
+        let accepted_start_length = accepted.iter().filter(|&&x| x < offset).count();
+
+        let accepted_start = Vec::from(&accepted[..accepted_start_length]);
+        let accepted_cycle = Vec::from_iter(
+            (&accepted[accepted_start_length..])
+                .iter()
+                .map(|&x| x - offset),
+        );
+
+        Self {
+            offset,
+            length,
+            accepted_start,
+            accepted_cycle,
+        }
+    }
+
+    fn is_accepted(&self, x: u64) -> bool {
+        if x < self.offset {
+            self.accepted_start.binary_search(&x).is_ok()
+        } else {
+            let x = (x - self.offset) % self.length;
+            self.accepted_cycle.binary_search(&x).is_ok()
+        }
+    }
+
+    fn first_accepted(&self) -> Option<u64> {
+        if let Some(first) = self.accepted_start.first() {
+            return Some(*first);
+        }
+
+        if let Some(first) = self.accepted_cycle.first() {
+            return Some(*first + self.offset);
+        }
+
+        None
+    }
+
+    fn union(&self, other: &Self) -> Self {
+        if other.offset > self.offset {
+            return other.union(self);
+        }
+
+        let offset = self.offset;
+        let (
+            ExtendedGcd {
+                gcd: d,
+                x: c1,
+                y: c2,
+            },
+            length,
+        ) = (self.length as i64).extended_gcd_lcm(&(other.length as i64));
+        let e1 = (c1 * self.length as i64).rem_euclid(length) as u64;
+        let e2 = (c2 * other.length as i64).rem_euclid(length) as u64;
+        let length = length as u64;
+
+        let accepted_start = self
+            .accepted_start
+            .iter()
+            .copied()
+            .filter(|&x| other.is_accepted(x))
+            .collect();
+
+        let accepted_cycle = self
+            .accepted_cycle
+            .iter()
+            .cartesian_product(other.accepted_cycle.iter())
+            .filter_map(|(&x1, &x2)| {
+                let a1 = (x1 + self.offset).rem_euclid(self.length);
+                let a2 = (x2 + other.offset).rem_euclid(other.length);
+                if a1 % d as u64 != 0 || a2 % d as u64 != 0 {
+                    return None;
+                }
+                let b1 = a1 / d as u64;
+                let b2 = a2 / d as u64;
+
+                let mut x = e2 * b1 + e1 * b2;
+                if x < offset {
+                    x += length;
+                }
+
+                (x - offset).rem_euclid(length).into()
+            })
+            .collect();
+
+        Self {
+            offset,
+            length,
+            accepted_start,
+            accepted_cycle,
+        }
+    }
+}
 
 pub struct Day08;
 impl Solution for Day08 {
-    type Input<'a> = ();
+    type Input<'a> = (Vec<Direction>, HashMap<u32, Node>);
 
     fn parse<'a>(content: &'a str) -> Self::Input<'a> {
         parsing::parse(content).unwrap().1
     }
 
-    fn part_a<'a>(_input: &Self::Input<'a>) -> String {
-        format!("{}", 0)
+    fn part_a<'a>(input: &Self::Input<'a>) -> String {
+        let mut current = Node::index("AAA");
+        let target = Node::index("ZZZ");
+
+        let directions = input.0.iter().cycle();
+
+        for (step, direction) in directions.enumerate() {
+            if current == target {
+                return format!("{}", step);
+            }
+            current = input.1[&current].get(*direction);
+        }
+
+        unreachable!()
     }
 
-    fn part_b<'a>(_input: &Self::Input<'a>) -> String {
-        format!("{}", 0)
+    fn part_b<'a>(input: &Self::Input<'a>) -> String {
+        let cycles = input
+            .1
+            .keys()
+            .filter(|&&n| Node::is_start(n))
+            .map(|&n| Cycle::trace(&input.0, &input.1, n))
+            .collect::<Vec<_>>();
+
+        let union_cycle = cycles
+            .into_iter()
+            .reduce(|x, y| Cycle::union(&x, &y))
+            .unwrap();
+
+        format!("{}", union_cycle.first_accepted().unwrap())
     }
 }
 
 mod parsing {
-    use nom::*;
+    use std::collections::HashMap;
 
-    pub(super) fn parse(input: &str) -> IResult<&str, ()> {
-        Ok((input, ()))
+    use super::Direction;
+    use super::Node;
+    use nom::{
+        bytes::complete::tag,
+        character::complete::char,
+        character::complete::{alphanumeric1, line_ending},
+        multi::{many1, separated_list1},
+        sequence::tuple,
+        *,
+    };
+
+    fn node(input: &str) -> IResult<&str, (u32, Node)> {
+        tuple((
+            alphanumeric1,
+            tag(" = ("),
+            alphanumeric1,
+            tag(", "),
+            alphanumeric1,
+            tag(")"),
+        ))
+        .map(|(x, _, l, _, r, _)| {
+            (
+                Node::index(x),
+                Node {
+                    left: Node::index(l),
+                    right: Node::index(r),
+                },
+            )
+        })
+        .parse(input)
+    }
+
+    fn direction(input: &str) -> IResult<&str, Direction> {
+        char('R')
+            .or(char('L'))
+            .map(|c| match c {
+                'R' => Direction::Right,
+                'L' => Direction::Left,
+                _ => unreachable!(),
+            })
+            .parse(input)
+    }
+
+    pub(super) fn parse(input: &str) -> IResult<&str, (Vec<Direction>, HashMap<u32, Node>)> {
+        many1(direction)
+            .and(many1(line_ending))
+            .and(separated_list1(line_ending, node))
+            .map(|((instr, _), nodes)| (instr, HashMap::from_iter(nodes.into_iter())))
+            .parse(input)
     }
 }
 
-// gen_test!(a, Day08, r"", "");
-// gen_test!(b, Day08, r"", "");
+gen_test!(
+    a,
+    Day08,
+    r"RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)",
+    "2"
+);
+gen_test!(
+    b,
+    Day08,
+    r"LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)",
+    "6"
+);
